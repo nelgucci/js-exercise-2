f035a3282a6b706c980613c4959e720ce3d08384
C410 <C410@AceLabs.lagcc>
2017-06-08 15:15:02 -0400

    try

--- js/app.js
+++ js/app.js
@@ -1,21 +1,51 @@
+
+
 var userSubmit = document.querySelector('#user-submit');
+
+// 	var userInput = document.querySelector('#user-input');
+// 	var toDoList = document.querySelector('#to-do-list');
+	
+
+
+// function myFunction() {
+//     var newItem = document.createElement("LI");
+//     var textnode = document.createTextNode("userInput");
+//     newItem.appendChild(textnode);
+
+//     var list = document.getElementById("toDoList");
+//     list.insertBefore(newItem, list.childNodes[0]);
+// }
+
 
 function addToDo(event){
 	event.preventDefault();
 	// console.log(event);
+
 	var userInput = document.querySelector('#user-input');
 	var toDoList = document.querySelector('#to-do-list');
+
 	// console.log(userInput.value);
 	// console.log(toDoList);
+
+	// var newItem = document.createElement("LI"); 
+	// var textnode = document.createTextNode(userInput.value);
+	// newItem.appendChild(textnode); 
+	// var list = document.getElementById("to-do-list");
+
 	if(userInput.value === ''){
 		return false; //return stops executing other codes underneath
 	}
 
-	toDoList.innerHTML += '<li>' + userInput.value + '</li>';
+	
+
+	toDoList.innerHTML = '<li>' + userInput.value + '</li>';
 
 	userInput.value = '';
 
 }
+
+
+
 
 userSubmit.addEventListener('click', addToDo, false);
 
