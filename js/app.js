aa3425f2330e0f5964d2816b1e1dabc40b80fcc7
nel gutierrez <ace_e303@e303-s24.lab.lagcc>
2017-06-09 16:45:01 -0400

    added close functionality

--- js/app.js
+++ js/app.js
@@ -5,7 +5,8 @@ var userSubmit = document.querySelector('#user-submit');
 // 	var userInput = document.querySelector('#user-input');
 // 	var toDoList = document.querySelector('#to-do-list');
 	
-
+	var userInput = document.querySelector('#user-input');
+	var toDoList = document.querySelector('#to-do-list');
 
 // function myFunction() {
 //     var newItem = document.createElement("LI");
@@ -33,8 +34,7 @@ function addToDo(event){
 
 	//----------------------------------------------
 
-	var userInput = document.querySelector('#user-input');
-	var toDoList = document.querySelector('#to-do-list');
+
 
 	// console.log(userInput.value);
 	// console.log(toDoList);
@@ -48,19 +48,29 @@ function addToDo(event){
 		return false; //return stops executing other codes underneath
 	}
 
-	
+toDoList.innerHTML = '<li><i class="fa fa-window-close close-to-do" aria-hidden="true"></i>' + userInput.value + '</li>' + toDoList.innerHTML;	
 
-toDoList.innerHTML = '<li>' + userInput.value + '<span class ="close">' +  "\u00D7" + '</span>' + '</li>' + toDoList.innerHTML;
+//toDoList.innerHTML = '<li>' + userInput.value + '<span class ="close">' +  "\u00D7" + '</span>' + '</li>' + toDoList.innerHTML;
 	//toDoList.innerHTML = '<li>' + userInput.value +  "\u00D7" + '</li>' + toDoList.innerHTML;
 
 	userInput.value = '';
 
 
+
 //----------------------------------------------------
-	var close = document.getElementsByClassName("close");
+//	var close = document.getElementsByClassName("close");
 }
 
+function removeToDo(event){
 
+	if(event.target.classList.contains('close-to-do'))
+	{
+		var li = event.target.parentElement;
+		toDoList.removeChild(li);
+	}
+}
+
+toDoList.addEventListener('click', removeToDo, false);
 
 userSubmit.addEventListener('click', addToDo, false);
 
